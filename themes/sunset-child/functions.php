ce8fa420ae620891ff6ededa76915323a77a1b15
Kauser Siddiqui <ksiddia80@gmail.com>
2024-12-03 12:15:25 -0500

    created css file

--- themes/sunset-child/functions.php
+++ themes/sunset-child/functions.php
@@ -15,3 +15,10 @@ function child_wp_enqueue_scripts() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'child_wp_enqueue_scripts' );
+
+function enqueue_logged_in_styles() {
+    if ( is_user_logged_in() ) {
+        wp_enqueue_style( 'logged-in-styles', get_stylesheet_directory_uri() . '/assets/app.css', array(), '1.0.0' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_logged_in_styles' );
